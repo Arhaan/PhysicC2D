eda4ddf686ae8b926b04bf7503ab83059e7c1716
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-21 23:25:37 +0530

    Add more functionality for collider, and make commented out functions for future use of OBBs

--- inc/collider.hpp
+++ inc/collider.hpp
@@ -1,14 +1,16 @@
 #include "boundingvolume.hpp"
 #include "glm/glm.hpp"
+#include <vector>
 class Collider{
     private:
             glm::vec2 m_position, m_scale; // m_position is the position of axis about which it is rotated, m_scale is a scaling factor in the two directions.
-            double rotation; //angle about which it is rotated about the z-axis;
+            double m_rotation; //angle about which it is rotated about the z-axis;
     public:
             Collider(glm::vec2 position = glm::vec2(0),
-                            double rotation = 0;
+                            double rotation = 0,
                             glm::vec2 scale = glm::vec2(1));
             virtual AABB getAABB() const = 0;
+            /* virtual OBB getOBB() const = 0; */
             virtual glm::vec2 getCentroid() const = 0;
 };
 
@@ -18,8 +20,9 @@ class BoxCollider: public Collider{
         public:
 
             BoxCollider(glm::vec2 position = glm::vec2(0),
-                            double rotation = 0;
+                            double rotation = 0,
                             glm::vec2 scale = glm::vec2(1));
             AABB getAABB();
+            /* OBB getOBB(); */
             glm::vec2 getCentroid();
 };
---
+++ src/collider.cpp
@@ -0,0 +1,11 @@
+#include "collider.hpp"
+
+Collider::Collider(glm::vec2 position, double rotation, glm::vec2 scale): m_position(position), m_rotation(rotation), m_scale(scale){}
+
+
+BoxCollider::BoxCollider(glm::vec2 position, double rotation, glm::vec2 scale):Collider(position, rotation, scale){
+        m_vertices[0] = glm::vec3(scale.x* 0.5f, scale.y*0.5f, 0);
+        m_vertices[1] = glm::vec3(-scale.x* 0.5f, scale.y*0.5f, 0);
+        m_vertices[2] = glm::vec3(scale.x* 0.5f, -scale.y*0.5f, 0);
+        m_vertices[3] = glm::vec3(-scale.x* 0.5f, -scale.y*0.5f, 0);
+}
