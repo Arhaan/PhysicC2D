f071ac6e84eedeb79e876c1817f3f10362a8a358
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-21 22:57:54 +0530

    Decide API for collider

---
+++ src/collider.hpp
@@ -0,0 +1,25 @@
+#include "boundingvolume.hpp"
+#include "glm/glm.hpp"
+class Collider{
+    private:
+            glm::vec2 m_position, m_scale; // m_position is the position of axis about which it is rotated, m_scale is a scaling factor in the two directions.
+            double rotation; //angle about which it is rotated about the z-axis;
+    public:
+            Collider(glm::vec2 position = glm::vec2(0),
+                            double rotation = 0;
+                            glm::vec2 scale = glm::vec2(1));
+            virtual AABB getAABB() const = 0;
+            virtual glm::vec2 getCentroid() const = 0;
+};
+
+class BoxCollider: public Collider{
+        private: 
+                std::vector<glm::vec3> m_vertices; // 2+1 dimensional
+        public:
+
+            BoxCollider(glm::vec2 position = glm::vec2(0),
+                            double rotation = 0;
+                            glm::vec2 scale = glm::vec2(1));
+            AABB getAABB();
+            glm::vec2 getCentroid();
+};
