55ed1154e2717e91ee0449f9b7f2a10203c56a0b
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-21 21:57:48 +0530

    Part of the earlier renaming

--- inc/bvh.hpp
+++ inc/bvh.hpp
@@ -1,28 +1,34 @@
 #include <vector>
-#include "boundingarea.hpp"
+#include "boundingvolume.hpp"
 #include "glm/glm.hpp"
 
 
-struct BAHNode{
-    BAHNode *parent = nullptr;
-    BAHNode *left = nullptr;
-    BAHNode *right = nullptr;
-    AABR area;
+struct BVHNode{
+    BVHNode *parent = nullptr;// Do we need this?
+    BVHNode *left = nullptr;
+    BVHNode *right = nullptr;
+    AABB volume;
+    bool is_leaf = false;
 };
 
 
 
-class BAH{
+class BVH{
         private:
-                std::vector<AABR> m_bounding_boxes;
-                BAHNode* m_head;
+                std::vector<AABB> m_bounding_boxes;
+                BVHNode* m_head;
                 void sort(int axis, int start, int end); // Sort according to axis (axis = 0 -> x, =1 -> y)
-                void buildTree(BAHNode* node, int start, int end); // Build the tree starting at node, and using only the AABRs in between start and end
+                void buildTreeTopDown(BVHNode* node, int start, int end); // Build the tree starting at node, and using only the AABRs in between start and end
                 int getAxisForCut(int start, int end); //By looking at the spread, get the axis cutting along which we'll get the more symmetric rectangles
-                AABR computeBA(int start, int end); // calculate the area that is occupied by the bounding areas between the indices start and end in the vector
-                int split(int start, int end); // might sort m_bounding_boxes and return the value at which to bisect the interval. The returned value has to be kept in the left interval
+                AABB computeBV(int start, int end); // calculate the area that is occupied by the bounding areas between the indices start and end in the vector
+                /* int split(int start, int end); // sort m_bounding_boxes and return the value at which to bisect the interval. The returned value has to be kept in the left interval */
+                enum Axis{
+                    X,
+                    Y,
+                    R, //Placeholder for random
+                };
         public:
-                BAH(std::vector<AABR> bounding_boxes): m_bounding_boxes(bounding_boxes){}
+                BVH(std::vector<AABB> bounding_boxes): m_bounding_boxes(bounding_boxes){}
                 void buildTree(){ // Build the tree
                         buildTree(m_head, 0, m_bounding_boxes.size() - 1);
                 }
