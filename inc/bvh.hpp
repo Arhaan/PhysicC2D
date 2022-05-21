c92276218ff842c0288dcbae89795f9b7715ea64
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-21 22:26:24 +0530

    Do some implementation of bvh

--- inc/bvh.hpp
+++ inc/bvh.hpp
@@ -17,19 +17,19 @@ class BVH{
         private:
                 std::vector<AABB> m_bounding_boxes;
                 BVHNode* m_head;
-                void sort(int axis, int start, int end); // Sort according to axis (axis = 0 -> x, =1 -> y)
-                void buildTreeTopDown(BVHNode* node, int start, int end); // Build the tree starting at node, and using only the AABRs in between start and end
-                int getAxisForCut(int start, int end); //By looking at the spread, get the axis cutting along which we'll get the more symmetric rectangles
-                AABB computeBV(int start, int end); // calculate the area that is occupied by the bounding areas between the indices start and end in the vector
-                /* int split(int start, int end); // sort m_bounding_boxes and return the value at which to bisect the interval. The returned value has to be kept in the left interval */
                 enum Axis{
                     X,
                     Y,
                     R, //Placeholder for random
                 };
+                int sort(int start, int end, Axis axis = Axis::R); // Sort according to axis (axis = 0 -> x, =1 -> y)
+                void buildTreeTopDown(BVHNode* node, int start, int end); // Build the tree starting at node, and using only the AABRs in between start and end
+                Axis getAxisForCut(int start, int end); //By looking at the spread, get the axis cutting along which we'll get the more symmetric rectangles
+                AABB computeBV(int start, int end); // calculate the area that is occupied by the bounding areas between the indices start and end in the vector
+                /* int split(int start, int end); // sort m_bounding_boxes and return the value at which to bisect the interval. The returned value has to be kept in the left interval */
         public:
                 BVH(std::vector<AABB> bounding_boxes): m_bounding_boxes(bounding_boxes){}
                 void buildTree(){ // Build the tree
-                        buildTree(m_head, 0, m_bounding_boxes.size() - 1);
+                        buildTreeTopDown(m_head, 0, m_bounding_boxes.size() - 1);
                 }
 };
---
+++ src/bvh.cpp
@@ -0,0 +1,79 @@
+#include "bvh.hpp"
+
+#include "glm/glm.hpp"
+
+int BVH::sort(int start, int end, BVH::Axis axis){
+    // Returns the index at which we split.
+    if(axis == Axis::R){
+        axis = getAxisForCut(start, end);
+    }
+    auto sort_func = [](AABB &a, AABB &b){
+            return(a.centre().x < b.centre().x);
+    };
+    
+    if(axis == Axis::Y){
+       // y axis
+        auto sort_func = [](AABB &a, AABB &b){
+            return(a.centre().y < b.centre().y);
+        }; 
+    }
+
+    std::sort(std::next(m_bounding_boxes.begin(), start),std::next(m_bounding_boxes.begin(), end+1) , sort_func);
+    return (start+(end-start)/2);
+}
+
+
+
+BVH::Axis BVH::getAxisForCut(int start, int end){
+        glm::vec2 min(m_bounding_boxes[start].centre());
+        glm::vec2 max(m_bounding_boxes[start].centre());
+        for(int i = start; i<= end; i++){
+            min = glm::min(min, m_bounding_boxes[i].centre());
+            max  = glm::max (max, m_bounding_boxes[i].centre());
+        }
+
+        glm::vec2 spreads = max-min;
+        if(spreads.x > spreads.y) return Axis::Y; // Return the axis with lower spread
+        else return Axis::X;
+}
+
+void BVH::buildTreeTopDown(BVHNode* node, int start, int end){
+    // We assume everything in node is unitialised at the start of this function, apart from node->parent
+
+    if(start == end){
+        // Leaf node
+        node->volume = m_bounding_boxes[start];
+        node->is_leaf = true;
+    }
+
+    else{
+            node->volume = computeBV(start, end);
+            int splitindex = sort(start, end);
+            BVHNode* leftNode = new BVHNode;
+            BVHNode* rightNode = new BVHNode;
+			node->left = leftNode;
+			node->right = rightNode;
+
+			leftNode->parent = node;
+			rightNode->parent = node;
+
+			buildTreeTopDown(leftNode, start, splitindex);
+			buildTreeTopDown(rightNode, splitindex + 1, end);
+
+    }
+    
+
+}
+
+
+
+AABB BVH::computeBV(int start, int end){
+        AABB bv = m_bounding_boxes[start];
+        for(int i = start+1; i<= end; i++){
+                bv = bv.getEnclosingBV(m_bounding_boxes[i]);
+        }
+
+        return bv;
+}
+
+
