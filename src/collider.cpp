a57b284ba08bf1dfa7963aa53bcb03d69214d4f2
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-22 10:09:35 +0530

    Initialise m_vertices in collider, otherwise we'll get segfault since m_vertices[0] may not be allocated

--- src/collider.cpp
+++ src/collider.cpp
@@ -4,8 +4,11 @@ namespace Physicc2D{
         Collider::Collider(glm::vec2 position, double rotation, glm::vec2 scale): m_position(position), m_rotation(rotation), m_scale(scale){}
 
 
-        BoxCollider::BoxCollider(glm::vec2 position, double rotation, glm::vec2 scale):Collider(position, rotation, scale){
-                m_vertices[0] = glm::vec3(scale.x* 0.5f, scale.y*0.5f, 0);
+        BoxCollider::BoxCollider(glm::vec2 position, double rotation, glm::vec2 scale):
+                Collider(position, rotation, scale), 
+                m_vertices(std::vector<glm::vec3>(8, glm::vec3(0, 0, 1.0f)))
+         {
+         m_vertices[0] = glm::vec3(scale.x* 0.5f, scale.y*0.5f, 0);
                 m_vertices[1] = glm::vec3(-scale.x* 0.5f, scale.y*0.5f, 0);
                 m_vertices[2] = glm::vec3(scale.x* 0.5f, -scale.y*0.5f, 0);
                 m_vertices[3] = glm::vec3(-scale.x* 0.5f, -scale.y*0.5f, 0);
