425a08133a5e1a9159c9f5f282f1bd6385c18f07
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-22 11:03:03 +0530

    Add a basic test to see everything works
    Outputs as expected

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
+#include "rigidbody.hpp"
+#include "bvh.hpp"
+#include "glm/glm.hpp"
+#include "boundingvolume.hpp"
+#include <vector>
+#include <iostream>
+using namespace std;
 int main()
 {
+        Physicc2D::RigidBody b1("b1");
+        Physicc2D::RigidBody b2("b2", glm::vec2(0.5, 0));
+        std::vector<Physicc2D::RigidBody> rb_list;
+        rb_list.push_back(b1);
+        rb_list.push_back(b2);
+        cout << "RB_list created"  << endl;
+        Physicc2D::BVH bvh(rb_list);
+        bvh.buildTree();
+        cout << "Tree built"  << endl;
+        bvh.ReportCollisionsDFS();
+
 	return 0;
 }
