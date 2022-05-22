52087b2871e6f40d4bf17e9bd64d356ede621be5
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-22 11:00:41 +0530

    Fix an error and start returning a basic AABB, without transforms

--- src/collider.cpp
+++ src/collider.cpp
@@ -6,7 +6,7 @@ namespace Physicc2D{
 
         BoxCollider::BoxCollider(glm::vec2 position, double rotation, glm::vec2 scale):
                 Collider(position, rotation, scale), 
-                m_vertices(std::vector<glm::vec3>(8, glm::vec3(0, 0, 1.0f)))
+                m_vertices(std::vector<glm::vec3>(4, glm::vec3(0, 0, 1.0f)))
          {
          m_vertices[0] = glm::vec3(scale.x* 0.5f, scale.y*0.5f, 0);
                 m_vertices[1] = glm::vec3(-scale.x* 0.5f, scale.y*0.5f, 0);
@@ -15,7 +15,7 @@ namespace Physicc2D{
         }
 
         AABB BoxCollider::getAABB() const{
-               return AABB(); // TODO
+               return AABB( glm::vec2(m_vertices[3].x , m_vertices[3].y)+m_position, glm::vec2(m_vertices[0].x, m_vertices[0].y)+m_position); // TODO: Add the transforms
         }
 
         glm::vec2 BoxCollider::getCentroid() const{
